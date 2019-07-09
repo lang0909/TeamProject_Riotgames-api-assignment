26cef633ee3570c72bcd072f574e77a034a1f49c
vihwan <vihwan@ajou.ac.kr>
2019-07-09 17:40:37 +0900

    Initialize Spring Boot Project

---
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/RiotgamesapiassignmentApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotgamesapiassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotgamesapiassignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/RiotgamesapiassignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RiotgamesapiassignmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
