bb9db6cce4f4178b583f42f452ee3e660066b839
vihwan <vihwan@ajou.ac.kr>
2019-07-10 18:24:52 +0900

    Update SummonerLeagueInfo Property

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/domain/SummonerLeagueInfo.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/domain/SummonerLeagueInfo.java
@@ -1,22 +1,23 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain;
 
-
 import lombok.Data;
 
 @Data
 public class SummonerLeagueInfo {
 
+    private String leagueId;
     private String queueType;
-    private String summonerName;
-    private boolean hotStreak;
-    private int wins;
-    private boolean veteran;
-    private int losses;
-    private String rank;
     private String tier;
+    private String rank;
+    private String summonerId;
+    private String summonerName;
+    private int leaguePoints;
+    private int wins;
+    private int losses;
+    private boolean veteran;
     private boolean inactive;
     private boolean freshBlood;
-    private String leagueId;
-    private String summonerId;
-    private int leaguePoints;
+    private boolean hotStreak;
+
+
 }
