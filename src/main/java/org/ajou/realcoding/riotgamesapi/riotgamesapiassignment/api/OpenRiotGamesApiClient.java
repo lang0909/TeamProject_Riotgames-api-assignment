1c1e3cf3924fcc7b0129d32a1aba38bb33a8a40d
vihwan <vihwan@ajou.ac.kr>
2019-07-10 18:22:49 +0900

    Update apiKey & apiUrl

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/api/OpenRiotGamesApiClient.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/api/OpenRiotGamesApiClient.java
@@ -10,16 +10,21 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class OpenRiotGamesApiClient {
 
-    private final String appid = ""; //Api Key
-    private final String openRiotGamesApiUrl = "";
+    private final String appid = "RGAPI-016e5e31-63ea-4dd5-a2e9-66ee1d8b908f"; //Api Key
+    private final String openRiotGamesApiUrl = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
+
+
+    /**
+     * Response 주소 양식
+     * https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/RiotSchmick?api_key=<key>
+     * */
+
 
     @Autowired
     RestTemplate restTemplate;
-
     public SummonerId requestSummonerId(String summonerId) {
         //exchange method
         return restTemplate.exchange(openRiotGamesApiUrl, HttpMethod.GET,null, SummonerId.class, summonerId, appid)
                 .getBody(); //응답받은 response의 body를 꺼내서 리턴
     }
-
 }
