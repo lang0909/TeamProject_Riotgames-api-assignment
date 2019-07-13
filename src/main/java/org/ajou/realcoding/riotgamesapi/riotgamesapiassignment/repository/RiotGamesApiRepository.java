a09d8198e4bd214e2cc4ba718d34fecc951683db
lang0909 <lang0909@ajou.ac.kr>
2019-07-13 16:27:35 +0900

    Modify RiotGamesApiRepository

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
@@ -4,6 +4,9 @@ import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.SummonerEn
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.AccumulatorOperators;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 
@@ -17,7 +20,15 @@ public class RiotGamesApiRepository
     {
         for(int i=0;i<summonerEntry.length;i++)
         {
-            mongoTemplate.insert(summonerEntry[i]);
+            Query query = Query.query((Criteria.where("summonerName").is(summonerEntry[i].getSummonerName())));
+            if(mongoTemplate.findOne(query,SummonerEntry.class)==null)
+            {
+                mongoTemplate.insert(summonerEntry[i]);
+            }
+            else
+            {
+                mongoTemplate.findAndReplace(query,summonerEntry[i]);
+            }
         }
     }
 
