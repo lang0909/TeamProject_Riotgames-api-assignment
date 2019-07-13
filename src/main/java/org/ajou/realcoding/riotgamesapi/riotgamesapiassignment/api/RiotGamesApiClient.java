7ae2e48b97dd938205781ecbb2f4e59aa71b00ac
lang0909 <lang0909@ajou.ac.kr>
2019-07-13 15:18:58 +0900

    Modify RiotGamesApiClient

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/api/RiotGamesApiClient.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/api/RiotGamesApiClient.java
@@ -7,12 +7,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
 @Service
 public class RiotGamesApiClient
 {
-    private final String apikey = "RGAPI-ea91a287-ed96-4035-b76a-fbae0f2da1bc";
-    private final String name_url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key={apikey}";
-    private final String entry_url = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryted_name}?api_key={apikey}";
+    private final String apikey = "RGAPI-ba0b9cf3-c26a-4894-ad3e-584b31fb3cd4";
+    private final String name_url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{user_name}?api_key={apikey}";
+    private final String entry_url = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encrytedId}?api_key={apikey}";
 
     @Autowired
     RestTemplate restTemplate;
@@ -22,9 +23,9 @@ public class RiotGamesApiClient
         return restTemplate.exchange(name_url, HttpMethod.GET,null,SummonerName.class,user_name,apikey).getBody();
     }
 
-    public SummonerEntry requestSummonerEntry(String encrytedId)
+    public SummonerEntry[] requestSummonerEntry(String encrytedId)
     {
-        return restTemplate.exchange(entry_url,HttpMethod.GET,null,SummonerEntry.class,encrytedId,apikey).getBody();
+        return restTemplate.exchange(entry_url,HttpMethod.GET,null,SummonerEntry[].class,encrytedId,apikey).getBody();
     }
 
 }
