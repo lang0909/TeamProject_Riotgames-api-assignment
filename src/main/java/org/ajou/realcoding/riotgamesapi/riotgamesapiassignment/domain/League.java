4b3caf951dc3552c2db7eccdb79aab3f44bb6ec9
do02reen24 <do02reen24@gmail.com>
2019-07-17 19:42:12 +0900

    Add League domain information

---
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/domain/League.java
@@ -0,0 +1,9 @@
+package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain;
+
+public class League {
+    private String queueType;
+    private String summonerName;
+    private boolean hotStreak;
+    private int wins;
+    private boolean veteran;
+}
