dae5bbb46c98fd512a72708a144cb4e1c971bbba
vihwan <vihwan@ajou.ac.kr>
2019-07-17 19:13:37 +0900

    Rename Service Name & Return Type

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/RiotgamesapiassignmentApplication.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/RiotgamesapiassignmentApplication.java
@@ -2,8 +2,10 @@ package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RiotgamesapiassignmentApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/ApiController.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/ApiController.java
@@ -1,37 +1,24 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.controller;
 
-import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.SummonerId;
-import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.SummonerLeagueInfo;
-import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.service.ApiService;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.Summoner;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.service.RiotGamesApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 public class ApiController {
 
     @Autowired
-    ApiService apiService;
-
-
-//    @GetMapping("/riotgamesapi/available-summonerids")
-//    public List<String> getAvailableSummonerIds() throws IOException {
-//
-//        return apiService.loadAvailableSummonerNameFromFile();
-//    }
-
-    @GetMapping("/riotgamesapi/SummonerId/byEncryptedId/{id}")
-    public SummonerId getSummonerId(@PathVariable String id){
-        return apiService.getSummonerIdById(id);
-    }
+    RiotGamesApiService riotGamesApiService;
 
     @GetMapping("/riotgamesapi/SummonerId/searchLeagueInfo/{id}")
-    public SummonerLeagueInfo getSummonerLeagueInfo(@PathVariable String id) {
-        return apiService.getSummonerLeagueInfo(id);
+    public League getSummonerLeagueInfo(@PathVariable String summonerName) {
+        return riotGamesApiService.getSummonerLeagueInfo(summonerName);
     }
 
 }
