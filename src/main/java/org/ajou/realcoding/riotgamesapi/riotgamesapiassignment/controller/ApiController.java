339617128130111e98953b629684d56f75f75473
vihwan <vihwan@ajou.ac.kr>
2019-07-17 19:46:20 +0900

    Optimized Import Files

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/ApiController.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/ApiController.java
@@ -1,14 +1,11 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.controller;
 
 import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
-import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.Summoner;
 import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.service.RiotGamesApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 public class ApiController {
