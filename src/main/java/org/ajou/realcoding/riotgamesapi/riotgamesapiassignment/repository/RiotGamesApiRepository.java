34d8a8fd4777c98a3b0c36150cc3a22a60b773e5
vihwan <vihwan@ajou.ac.kr>
2019-07-17 19:47:22 +0900

    Update Inserting & Updating Log Comment

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
@@ -1,27 +1,30 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
-import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.Summoner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
-
+@Slf4j
 @Repository
 public class RiotGamesApiRepository {
     @Autowired
     MongoTemplate mongoTemplate;
 
     public void insertAndUpdateRiotGamesApi(League[] league) {
+
         for (int i = 0; i < league.length; i++) {
             Query query = Query.query((Criteria.where("summonerName").is(league[i].getSummonerName())));
             if (mongoTemplate.findOne(query, League.class) == null) {
                 mongoTemplate.insert(league[i]);
+                log.info("< Inserting Hide on bush's League Information. Please wait. >");
             } else {
                 //TODO
-//                mongoTemplate.update(); :
+                mongoTemplate.findAndReplace(query, league[i]);
+                log.info("< Updating Hide on bush's League Information. Please wait. >");
             }
         }
     }
