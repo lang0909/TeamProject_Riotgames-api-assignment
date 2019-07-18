eec09f3e941dcf92558090b797542f2f63fe093e
do02reen24 <do02reen24@gmail.com>
2019-07-18 21:33:02 +0900

    Add getLeagueBySummonerName method in Controller

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/SummonerController.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/SummonerController.java
@@ -1,14 +1,23 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.controller;
 
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.api.RiotGamesApiClient;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
 import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.service.RiotGamesApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SummonerController {
-
     @Autowired
     RiotGamesApiService riotGamesApiService;
+
+   @Autowired
+    RiotGamesApiClient riotGamesApiClient;
+
+    @GetMapping("/league/{summonerName}")
+    public League getLeagueBySummonerName(@PathVariable String summonerName){
+        League league = riotGamesApiService.getLeagueBySummoner(summonerName);
+        return league;
+    }
 
 }
