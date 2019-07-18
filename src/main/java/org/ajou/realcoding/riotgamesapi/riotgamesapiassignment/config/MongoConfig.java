a4d0e5c0d4a290aca9828054fca8a5eadf05bc80
jinyoung <bmdj123@gmail.com>
2019-07-19 03:24:33 +0900

    Add dependency in pom.xml and add createMongoTemplate method in MongoConfig.java

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/config/MongoConfig.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/config/MongoConfig.java
@@ -1,4 +1,14 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.config;
 
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
 public class MongoConfig {
+    @Bean
+    public MongoTemplate createMongoTemplate(){
+        return new MongoTemplate(new MongoClient(),"riot-games-api");
+    }
 }
