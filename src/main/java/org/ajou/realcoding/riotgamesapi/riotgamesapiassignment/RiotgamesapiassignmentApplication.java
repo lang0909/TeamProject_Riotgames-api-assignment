c91c53ba3460a7b6adfda617fff5ba9080616f78
do02reen24 <do02reen24@gmail.com>
2019-07-18 20:15:14 +0900

    Add application code

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/RiotgamesapiassignmentApplication.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/RiotgamesapiassignmentApplication.java
@@ -1,5 +1,15 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
 public class RiotgamesapiassignmentApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(RiotgamesapiassignmentApplication.class, args);
+    }
 
 }
