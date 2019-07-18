dad3cbf28f2a2c1bceb2049eca112bf60692a72c
jinyoung <bmdj123@gmail.com>
2019-07-19 04:12:51 +0900

    add method in RiotGamesApiController.java

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/RiotGamesApiController.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/RiotGamesApiController.java
@@ -1,4 +1,20 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.controller;
 
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.service.RiotGamesApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RiotGamesApiController {
+    @Autowired
+    RiotGamesApiService riotGamesApiService;
+
+    @GetMapping("/league/{summonerName}")
+    public League getLeagueFromSummonerName(@PathVariable String summonerName){
+        return riotGamesApiService.getLeagueInfo(summonerName);
+    }
+
 }
