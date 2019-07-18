2524b3b0b87a7548bf325eaf4dc268bacaeb00f5
do02reen24 <do02reen24@gmail.com>
2019-07-18 15:27:13 +0900

    Add League component

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/domain/League.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/domain/League.java
@@ -1,9 +1,20 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain;
 
+import lombok.Data;
+
+@Data
 public class League {
     private String queueType;
     private String summonerName;
     private boolean hotStreak;
     private int wins;
     private boolean veteran;
+    private int losses;
+    private String rank;
+    private String leagueId;
+    private boolean inactive;
+    private boolean freshBlood;
+    private String tier;
+    private String summoerId;
+    private int leaguePoints;
 }
