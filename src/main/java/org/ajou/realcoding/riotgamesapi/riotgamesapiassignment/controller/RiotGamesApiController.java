1e365532275e57d2dd09cccd7377a2c02d5d9476
vihwan <vihwan@ajou.ac.kr>
2019-07-18 17:24:32 +0900

    Rename ApiController

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/RiotGamesApiController.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/RiotGamesApiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ApiController {
+public class RiotGamesApiController {
 
     @Autowired
     RiotGamesApiService riotGamesApiService;
