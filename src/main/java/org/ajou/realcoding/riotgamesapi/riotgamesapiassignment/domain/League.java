00520aef176a45f01f51a9ffd7d5634c3b9d94b3
jinyoung <bmdj123@gmail.com>
2019-07-18 23:00:59 +0900

    add variables in League.java and Summoner.java

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/domain/League.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/domain/League.java
@@ -1,4 +1,23 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain;
 
+import lombok.Data;
+
+@Data
+
 public class League {
+
+    private String queueType;
+    private String summonerName;
+    private boolean hotStreak;
+    private int wins;
+    private boolean veteran;
+    private int losses;
+    private String rank;
+    private String leagueId;
+    private boolean inactive;
+    private boolean freshBlood;
+    private String tier;
+    private String summonerId;
+    private int leaguePoints;
+
 }
--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/domain/Summoner.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/domain/Summoner.java
@@ -1,4 +1,14 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain;
 
+import lombok.Data;
+
+@Data
 public class Summoner {
+    private int profileIconId;
+    private String name;
+    private String puuid;
+    private long summonerLevel;
+    private long revisionDate;
+    private String id;
+    private String accountId;
 }
