19e9e8ec6b7c324cc63d9b456e59b72675f663c5
jinyoung <bmdj123@gmail.com>
2019-07-19 03:56:44 +0900

    Add method in RiotGamesApiService.java

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/service/RiotGamesApiService.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/service/RiotGamesApiService.java
@@ -3,15 +3,36 @@ package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.service;
 import lombok.extern.slf4j.Slf4j;
 import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.api.RiotGamesApiClient;
 import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.Summoner;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.repository.RiotGamesApiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @Slf4j
 public class RiotGamesApiService {
 
+    @Autowired
+    RiotGamesApiClient riotGamesApiClient;
+
+    @Autowired
+    RiotGamesApiRepository riotGamesApiRepository;
+
+    public League getLeagueInfo(String userName){
+        return riotGamesApiRepository.findLeagueInfo(userName);
+    }
+
+    @Scheduled(fixedDelay = 2000L)
+    public void getRiotGamesApiPeriodically(){
+
+        Summoner summoner = riotGamesApiClient.requestSumonerInfo("hide on bush");
+        League league = riotGamesApiClient.requestLeagueInfo(summoner.getId());
+        riotGamesApiRepository.insertRiotGamesApi(league);
+        log.info("riot games api has been inserted successfully. {}",league);
+
+    }
 
 }
 
