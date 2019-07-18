f50c204ff64f057a7d68cb11c7e8a6d9cb73eb30
jinyoung <bmdj123@gmail.com>
2019-07-19 03:30:18 +0900

    Add annotation in RiotGamesApiService.java

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/service/RiotGamesApiService.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/service/RiotGamesApiService.java
@@ -1,4 +1,17 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.api.RiotGamesApiClient;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
 public class RiotGamesApiService {
+
+
 }
+
