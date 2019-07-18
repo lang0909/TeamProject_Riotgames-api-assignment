f919f7b9a44c2eafa8a87ddbd11a0747a99530e7
jinyoung <bmdj123@gmail.com>
2019-07-19 03:40:12 +0900

    Add insertRiotGamesApi in RiotGamesApiRepository.java

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
@@ -1,4 +1,21 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.repository;
 
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
 public class RiotGamesApiRepository {
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public void insertRiotGamesApi(League[] league){
+        Query query = Query.query(Criteria.where("summonerName").is(league[0].getSummonerId()));
+        mongoTemplate.insert(league[0]);
+    }
 }
