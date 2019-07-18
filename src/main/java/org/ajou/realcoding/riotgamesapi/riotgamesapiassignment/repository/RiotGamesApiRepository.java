ea43ffeceacc47b48169d1c60f441f229292197f
jinyoung <bmdj123@gmail.com>
2019-07-19 03:44:37 +0900

    add findLeagueInfo method in RiotGamesApiRepository.java

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
@@ -1,6 +1,7 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.repository;
 
 import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.Summoner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -14,6 +15,10 @@ public class RiotGamesApiRepository {
     @Autowired
     MongoTemplate mongoTemplate;
 
+    public League findLeagueInfo(String summonername){
+        Query query = Query.query(Criteria.where("name").is(summonername));
+        return mongoTemplate.findOne(query, League.class);
+    }
     public void insertRiotGamesApi(League[] league){
         Query query = Query.query(Criteria.where("summonerName").is(league[0].getSummonerId()));
         mongoTemplate.insert(league[0]);
