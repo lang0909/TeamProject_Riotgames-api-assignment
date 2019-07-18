6ac16c7b528477f44e1832b039ea1a1ad17ab5bd
do02reen24 <do02reen24@gmail.com>
2019-07-18 21:32:14 +0900

    Add insertLeague method in repository

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/repository/RiotGamesApiRepository.java
@@ -1,9 +1,11 @@
 package org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.repository;
 
+import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.League;
+//import org.ajou.realcoding.riotgamesapi.riotgamesapiassignment.domain.Summoner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
+//import org.springframework.data.mongodb.core.query.Criteria;
+//import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,6 +13,11 @@ public class RiotGamesApiRepository {
     @Autowired
     MongoTemplate mongoTemplate;
 
+    public void insertLeague(League league){ mongoTemplate.insert(league);};
 
+//    public League findLeague(String encryptedid){
+//        Query query = Query.query(Criteria.where("id").is(encryptedid));
+//        return mongoTemplate.findOne(query,League.class);
+//    }
 }
 
