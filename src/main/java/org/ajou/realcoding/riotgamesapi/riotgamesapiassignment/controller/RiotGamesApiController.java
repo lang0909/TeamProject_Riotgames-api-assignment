e26f11b69974ede667e2f005f75f236b9ccf0c5f
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-18 17:26:53 +0900

    Change Name of Controller

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/RiotGamesApiController.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/controller/RiotGamesApiController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SummonerController {
+public class RiotGamesApiController {
 
     @Autowired
     RiotGamesApiService riotGamesApiService;
