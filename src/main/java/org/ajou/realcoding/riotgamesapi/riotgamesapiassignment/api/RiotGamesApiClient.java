30a4faedf51590978d34556accff834c09a03289
do02reen24 <do02reen24@gmail.com>
2019-07-18 22:24:22 +0900

    Fixed typos in apiclient

--- src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/api/RiotGamesApiClient.java
+++ src/main/java/org/ajou/realcoding/riotgamesapi/riotgamesapiassignment/api/RiotGamesApiClient.java
@@ -11,8 +11,8 @@ import org.springframework.web.client.RestTemplate;
 public class RiotGamesApiClient {
 
     private final String api_key = "RGAPI-69dac4d4-ed0f-49f2-9b86-af7c126d094d";
-    private final String summonerUrl = "https://kr.api.riotgames.com/lol/summoners/v4/summoners/by-name/{summonerName}?api_key={apikey}";
-    private final String LeagueUrl ="https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={apikey}";
+    private final String summonerUrl = "https://kr.api.riotgames.com/lol/summoners/v4/summoners/by-name/{summonerName}?api_key={api_key}";
+    private final String LeagueUrl ="https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={api_key}";
 
     @Autowired
     RestTemplate restTemplate;
@@ -21,9 +21,8 @@ public class RiotGamesApiClient {
         return summoner;
     }
 
-    public League getLeague(String encryptedSummonerId) {
-        League league = restTemplate.exchange(LeagueUrl,HttpMethod.GET,null,League.class,encryptedSummonerId,api_key).getBody();
-        return league;
+    public League[] getLeague(String encryptedSummonerId) {
+        return restTemplate.exchange(LeagueUrl,HttpMethod.GET,null,League[].class,encryptedSummonerId,api_key).getBody();
     }
 
 }
